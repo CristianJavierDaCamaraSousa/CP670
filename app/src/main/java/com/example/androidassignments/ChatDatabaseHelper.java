c81a86e2b7e35d3ce6eccc25c6009d5274199190
Cristian Da Camara <cristianjdcs@gmail.com>
2025-07-01 18:11:14 -0400

    Assgn 3.1 ready. Testing missing

---
+++ app/src/main/java/com/example/androidassignments/ChatDatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.androidassignments;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "chat.db";
+    private static final int VERSION_NUM = 1;
+
+    public static final String TABLE_NAME = "messages";
+    public static final String KEY_ID ="_id";
+    public static final String KEY_MESSAGE = "message";
+
+    private static final String DATABASE_CREATE= "CREATE TABLE "
+            + TABLE_NAME + "(" + KEY_ID
+            + " integer primary key autoincrement, " + KEY_MESSAGE
+            + " text not null);";
+
+    private static final String DATABASE_DROP = "DROP TABLE IF EXISTS "+ TABLE_NAME;
+
+    public ChatDatabaseHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVersion + " newVersion=" + newVersion);
+        db.execSQL(DATABASE_DROP); //I drop the table and then call onCreate to update it
+        onCreate(db);
+
+    }
+
+
+}
--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -1,7 +1,12 @@
 package com.example.androidassignments;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,11 +27,14 @@ import java.util.ArrayList;
 
 public class ChatWindow extends AppCompatActivity {
 
+    private static final String ACTIVITY_NAME = "ChatWindow" ;
     ListView listViewChatWindow;
     EditText editTextChat;
     Button buttonSend;
     ArrayList<String> messages;
     ChatAdapter messageAdapter;
+    private ChatDatabaseHelper dhHelper;
+    private SQLiteDatabase myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,40 @@ public class ChatWindow extends AppCompatActivity {
         messages = new ArrayList<>();
         messageAdapter = new ChatAdapter( this,messages );
         listViewChatWindow.setAdapter(messageAdapter);
+
+        /*
+        * DataBase
+        * */
+
+        dhHelper = new ChatDatabaseHelper(this);
+        myDB = dhHelper.getWritableDatabase();
+
+        Cursor cur = myDB.rawQuery("SELECT * FROM " + ChatDatabaseHelper.TABLE_NAME,null);
+
+
+        //TODO CHECK LATER
+
+        Log.i(ACTIVITY_NAME, "Cursor’s column count = " + cur.getColumnCount());
+        for (int i = 0; i < cur.getColumnCount(); i++) {
+            Log.i(ACTIVITY_NAME, "Column name: " + cur.getColumnName(i));
+        }
+
+        int messageColIndex = cur.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE);
+        if (messageColIndex != -1) {
+            cur.moveToFirst();
+            while (!cur.isAfterLast()) {
+                Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + cur.getString(messageColIndex));
+                messages.add(cur.getString(messageColIndex));
+                cur.moveToNext();
+            }
+        }
+
+
+
+
+
+        cur.close();
+
     }
 
     private class ChatAdapter extends ArrayAdapter<String>{
@@ -75,13 +117,45 @@ public class ChatWindow extends AppCompatActivity {
 
 
     public void sendMessage(View view){
+        //use it to insert values into the DB
+        ContentValues cValues = new ContentValues();
+
         String newMessage = editTextChat.getText().toString();
         if(!newMessage.isEmpty()){
             messages.add(newMessage);
             messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/getView()
             editTextChat.setText("");
+            cValues.put(ChatDatabaseHelper.KEY_MESSAGE,newMessage);
+            myDB.insert(ChatDatabaseHelper.TABLE_NAME,null,cValues);
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myDB.close();
+    }
+
+    /*
+    *   ContentValues Cvalues = new ContentValues();
+        cValues.put(“FirstName”,  “Abdul-Rahman”);
+        cValues.put(“LastName”,  “Mawlood-Yunis”);
+        cValues.put(“email”,  “amawloodyunis@wlu.ca” );
+
+        dataBaseName.insert(“TableName”, “NullPlaceholder”, cValues)
+        *
+        *
+        *
+        * public Item createItem(Item item) {
+
+            ContentValues values = new ContentValues();
+            values.put(SQLiteHelper.COLUMN_ITEM, item.getItem());
+
+            long insertId = database.insert(SQLiteHelper.TABLE_ITEMS, null,
+                values);
+…
+}
+    *
+    * */
 
 }
