0d4a5cc1b827a74d570c5185d8db447847b5a4ba
Cristian Da Camara <cristianjdcs@gmail.com>
2025-07-16 19:07:16 -0400

    Kind of working

--- app/src/main/java/com/example/androidassignments/MessageDetails.java
+++ app/src/main/java/com/example/androidassignments/MessageDetails.java
@@ -9,29 +9,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MessageDetails extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_message_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        if (savedInstanceState == null) {
-            Bundle args = new Bundle();
-            args.putString("message", getIntent().getStringExtra("message"));
-            args.putLong("id", getIntent().getLongExtra("id", -1));
+        Bundle dataToPass = getIntent().getExtras();
 
-            MessageFragment fragment = new MessageFragment();
-            fragment.setArguments(args);
+        MessageFragment fragment = new MessageFragment(null);
+        fragment.setArguments(dataToPass);
 
-            getSupportFragmentManager().beginTransaction()
-                    //.replace(R.id.frameLayoutDetails, fragment)
-                    .commit();
-        }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.message_details_frame_layout, fragment)
+                .commit();
     }
 }
--- app/src/main/java/com/example/androidassignments/MessageFragment.java
+++ app/src/main/java/com/example/androidassignments/MessageFragment.java
@@ -1,5 +1,6 @@
 package com.example.androidassignments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,78 +11,53 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.Activity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MessageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+
 public class MessageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ChatWindow parentActivity;
 
     public MessageFragment() {
-        // Required empty public constructor
+        // Necesario para reconstrucción automática
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessageDetailsFrag.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MessageFragment newInstance(String param1, String param2) {
-        MessageFragment fragment = new MessageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public MessageFragment(ChatWindow activity) {
+        parentActivity = activity;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_message_details, container, false);
-        TextView textMessage = view.findViewById(R.id.fragmentMessage);
-        TextView textMessageId = view.findViewById(R.id.fragmentMessageId);
-        Button buttonDelete = view.findViewById(R.id.fragmentDeleteButton);
-
 
         Bundle args = getArguments();
-        if (args != null) {
-            String message = args.getString("message", "No message received");
-            int id = args.getInt("id", -1);
+        String message = args.getString("message");
+        long id = args.getLong("id");
 
-            textMessage.setText(message);
-            textMessageId.setText("ID= " + id);
-        }
+        TextView msgText = view.findViewById(R.id.fragmentMessage);
+        TextView idText = view.findViewById(R.id.fragmentMessageId);
+        Button deleteButton = view.findViewById(R.id.fragmentDeleteButton);
 
+        msgText.setText(message);
+        idText.setText("ID: " + id);
 
-        buttonDelete.setOnClickListener(v -> {
-            Toast.makeText(getActivity(), "Mensaje eliminado", Toast.LENGTH_SHORT).show();
-            // Aquí podrías llamar a una función para eliminar el mensaje de una lista o BD
+        deleteButton.setOnClickListener(v -> {
+            if (parentActivity != null) {
+                //parentActivity.deleteMessageById(id);
+                getParentFragmentManager().beginTransaction().remove(this).commit();
+            } else {
+                Intent intent = new Intent();
+                intent.putExtra("id", id);
+                getActivity().setResult(Activity.RESULT_OK, intent);
+                getActivity().finish();
+            }
+
         });
+
         return view;
     }
+
+
 }
